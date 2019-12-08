f665e75e6861f8b1e0843f1ce72bf4bfcce61493
Francis Badasu <nytro04@gmail.com>
2019-12-08 15:48:48 +0000

    clear forms bug fix

--- src/components/NewMovie.js
+++ src/components/NewMovie.js
@@ -1,86 +1,137 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 // const container = {
 //     "alignItems" : "center"
 // }
 
+export default class NewMovie extends Component {
+  state = {
+    title: "",
+    year: "",
+    type: "",
+    poster: ""
+  };
 
-export default class NewMovie extends Component{
-   state = { 
-        title: "",
-        year: "",
-        type: "",
-        poster: ""
-}
-    
-    handleFormSubmit = (e) => {
-        e.preventDefault();
-       let movieDB = JSON.parse(localStorage.getItem("movie"))
-        movieDB = movieDB ? movieDB : []
-        const id = Math.floor(Math.random() * 10000)
-        const { title, year, type, poster } = this.state
-        const newMovie = { id, title, year,type, poster }
-        localStorage.setItem("movie", JSON.stringify([...movieDB, newMovie]))
-        this.setState({
-                    title: "",
-                    year: "",
-                    type: "",
-                    poster: ""
-})
-        console.log(newMovie) 
-    }
+  handleFormSubmit = e => {
+    e.preventDefault();
+    let movieDB = JSON.parse(localStorage.getItem("movie"));
+    movieDB = movieDB ? movieDB : [];
+    const id = Math.floor(Math.random() * 10000);
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name] : e.target.value
-        })
-    }
+    const { title, year, type, poster } = this.state;
 
-    render(){
-        return (
-        <div class="container" >
-            <div class="row">
-                <div class="col-md-12">
-                     <div class="well well-sm">
-                     <form class="form-horizontal"  onSubmit={this.handleFormSubmit}>
-                    <fieldset>
-                        <h4 class="text-left header"> New Movie</h4>
-                            <div class="form-group">
-                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
-                            <div class="col-md-8">
-                                <input id="title" name="title" type="text" placeholder="Movie Title" className="form-control" required onChange={this.handleChange}  />
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
-                            <div class="col-md-8">
-                                <input id="year" name="year" type="text" placeholder="Release Year" className="form-control" required onChange={this.handleChange}  />
-                            </div>
-                        </div>
+    const newMovie = { id, title, year, type, poster };
+    localStorage.setItem("movie", JSON.stringify([...movieDB, newMovie]));
 
-                        <div class="form-group">
-                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
-                            <div class="col-md-8">
-                                <input id="type" name="type" type="text" placeholder="Movie type" className="form-control" required onChange={this.handleChange} />
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
-                            <div class="col-md-8">
-                                <input id="type" name="poster" type="text" placeholder="Movie Poster" className="form-control" required onChange={this.handleChange}  />
-                            </div>
-                        </div>
+    this.setState({
+      title: "",
+      year: "",
+      type: "",
+      poster: ""
+    });
+    console.log(newMovie);
+  };
 
-                        <div class="form-group">
-                            <div class="col-md-12 text-center">
-                                <button type="submit" className="btn  btn-lg" >Add Movie</button>
-                            </div>
-                        </div>
-                    </fieldset>
-                </form>
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    const { title, year, type, poster } = this.state;
+
+    return (
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="well well-sm">
+              <form class="form-horizontal" onSubmit={this.handleFormSubmit}>
+                <fieldset>
+                  <h4 class="text-left header"> New Movie</h4>
+                  <div class="form-group">
+                    <span class="col-md-1 col-md-offset-2 text-center">
+                      <i class="fa fa-user bigicon"></i>
+                    </span>
+                    <div class="col-md-8">
+                      <input
+                        id="title"
+                        name="title"
+                        type="text"
+                        value={title}
+                        placeholder="Movie Title"
+                        className="form-control"
+                        required
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <span class="col-md-1 col-md-offset-2 text-center">
+                      <i class="fa fa-user bigicon"></i>
+                    </span>
+                    <div class="col-md-8">
+                      <input
+                        id="year"
+                        name="year"
+                        type="text"
+                        value={year}
+                        placeholder="Release Year"
+                        className="form-control"
+                        required
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <span class="col-md-1 col-md-offset-2 text-center">
+                      <i class="fa fa-envelope-o bigicon"></i>
+                    </span>
+                    <div class="col-md-8">
+                      <input
+                        id="type"
+                        name="type"
+                        type="text"
+                        value={type}
+                        placeholder="Movie type"
+                        className="form-control"
+                        required
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <span class="col-md-1 col-md-offset-2 text-center">
+                      <i class="fa fa-pencil-square-o bigicon"></i>
+                    </span>
+                    <div class="col-md-8">
+                      <input
+                        id="type"
+                        name="poster"
+                        type="text"
+                        value={poster}
+                        placeholder="Movie Poster"
+                        className="form-control"
+                        required
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="col-md-12 text-center">
+                      <button type="submit" className="btn  btn-lg">
+                        Add Movie
+                      </button>
+                    </div>
+                  </div>
+                </fieldset>
+              </form>
             </div>
+          </div>
         </div>
-    </div>
-</div>
-)}
+      </div>
+    );
+  }
 }
